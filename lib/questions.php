32980feed8cbfa1acf03f4da186f2a5c42829f8e
Dmitry Melamuud <balu@migdal.ru>
2012-01-08 15:26:04 +0200

    Bare HE to RU question added

--- lib/questions.php
+++ lib/questions.php
@@ -2,7 +2,8 @@
 # Question variants
 
 const QV_WORD_HE_RU = 1;
-const QV_WORD_RU_HE = 2;
-const QV_WORD_RU_HE_WRITE = 3;
-const QV_WORD_RU_HE_NEKUDOT = 4;
+const QV_WORD_BARE_HE_RU = 2;
+const QV_WORD_RU_HE = 3;
+const QV_WORD_RU_HE_WRITE = 4;
+const QV_WORD_RU_HE_NEKUDOT = 5;
 ?>

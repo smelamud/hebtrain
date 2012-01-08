208449fd11670d8e840ccd37864e40b9f55c0c07
Dmitry Melamuud <balu@migdal.ru>
2012-01-08 18:14:37 +0200

    Заголовок и меню

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Иврит</title>
+    <link rel="stylesheet" type="text/css" href="/css/common.css"/>
+  </head>
+  <body>
+    <header>
+      <div class="site-title">Иврит</div>
+      <ul class="menu"><?php
+      ?><li>Начало</li><?php
+      ?><li><a href="/run.php">Тест</a></li><?php
+      ?><li><a href="/add.php">Добавить</a></li><?php
+      ?><li><a href="/list.php">Список</a></li><?php
+    ?></ul>
+    </header>
+  </body>
+</html>

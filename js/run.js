b4655507e4106a25e20f687836487783836c6f2d
Dmitry Melamuud <balu@migdal.ru>
2012-01-19 00:00:37 +0200

    Задаем первый вопрос

--- js/run.js
+++ js/run.js
@@ -12,10 +12,14 @@ function shuffle(arr) {
 }
 
 function loadTest() {
+    $("#start").show();
+    $("#loading").show();
+    $("#loaded").hide();
+    $("#buttons-start").hide();
+
     $.getJSON("/actions/test-load.php",
         function(data) {
             window.testData = data;
-            shuffle(window.testData);
             $("#loading").hide();
             $("#loaded").show();
             $("#buttons-start").show();
@@ -28,11 +32,59 @@ function loadTest() {
 }
 
 function startTest() {
-    alert("Started!");
+    $("#start").hide();
+    $("#run").show();
+    shuffle(window.testData);
+    window.testCurrent = 0;
+    showQuestion();
+}
+
+function showQuestion() {
+    var title = $(".question-title-show");
+    title.removeClass("question-title-show");
+    title.addClass("question-title-hide");
+
+    var data = window.testData[window.testCurrent];
+    title = $("#question-title-" + data.question);
+    title.removeClass("question-title-hide");
+    title.addClass("question-title-show");
+
+    var word = '';
+    switch(data.question) {
+        case 1: // QV_WORD_HE_RU
+            word = data.hebrew;
+            comment = data.hebrew_comment;
+            break;
+        case 2: // QV_WORD_BARE_HE_RU
+            word = data.hebrew_bare;
+            comment = data.hebrew_comment;
+            break;
+        case 3: // QV_WORD_RU_HE
+        case 4: // QV_WORD_RU_HE_WRITE
+        case 5: // QV_WORD_RU_HE_NEKUDOT
+            word = data.russian;
+            comment = data.russian_comment;
+            break;
+    }
+    $("#question-word").text(word);
+    if (comment.length > 0) {
+        $("#question-comment").text("[" + comment + "]");
+    } else {
+        $("#question-comment").text("");
+    }
+
+    $("#buttons-answer").show();
+}
+
+function answered() {
+    $("#buttons-answer").hide();
+
+    var data = window.testData[window.testCurrent];
 }
 
 $(function() {
     $("#button-start").click(startTest);
+    $("#button-answer").click(answered);
     $("#main").ajaxStart(function() {
         $("#spinner").css("visibility", "visible");
     });
--- run.php
+++ run.php
@@ -15,11 +15,37 @@ require_once('parts/header.php');
         <?php displayMainMenu('run'); ?>
         <div id="main">
             <img id="spinner" src="/pics/ajax.gif"><br/>
-            <span id="loading">Загрузка теста</span>
-            <span id="loaded">Тест готов</span>
-            <p id="buttons-start">
-                <button id="button-start">Начать тест</button>
-            </p>
+            <div id="start">
+                <span id="loading">Загрузка теста</span>
+                <span id="loaded">Тест готов</span>
+                <p id="buttons-start">
+                    <button id="button-start">Начать тест</button>
+                </p>
+            </div>
+            <div id="run">
+                <span id="question-title-1" class="question-title-hide">
+                    Переведите на русский язык
+                </span>
+                <span id="question-title-2" class="question-title-hide">
+                    Переведите на русский язык
+                </span>
+                <span id="question-title-3" class="question-title-hide">
+                    Переведите на иврит
+                </span>
+                <span id="question-title-4" class="question-title-hide">
+                    Напишите на иврите без огласовок
+                </span>
+                <span id="question-title-5" class="question-title-hide">
+                    Напишите на иврите с огласовками
+                </span>
+                <p>
+                    <span id="question-word">&nbsp;</span>
+                    &#x200e;<span id="question-comment">&nbsp;</span>
+                </p>
+                <p id="buttons-answer">
+                    <button id="button-answer">Ответ</button>
+                </p>
+            </div>
         </div>
     </body>
 </html>

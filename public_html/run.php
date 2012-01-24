3e05c883f1abe9ac8caf79fea96b6e504eb5f687
Dmitry Melamuud <balu@migdal.ru>
2012-01-24 02:42:10 +0200

    Грубый и неправильный перевод run.php на bootstrap.

--- public_html/run.php
+++ public_html/run.php
@@ -18,28 +18,32 @@ require_once('parts/header.php');
             <div id="main" class="content">
                 <img id="spinner" src="/pics/ajax.gif"><br/>
                 <div id="start">
-                    <span id="loading">Загрузка теста</span>
-                    <span id="loaded">Тест готов</span>
+                    <h3>
+                        <span id="loading">Загрузка теста</span>
+                        <span id="loaded">Тест готов</span>
+                    </h3>
                     <p id="buttons-start">
                         <button id="button-start" class="btn primary">Начать тест</button>
                     </p>
                 </div>
                 <div id="run">
-                    <span id="question-title-1" class="question-title-hide">
-                        Переведите на русский язык
-                    </span>
-                    <span id="question-title-2" class="question-title-hide">
-                        Переведите на русский язык
-                    </span>
-                    <span id="question-title-3" class="question-title-hide">
-                        Переведите на иврит
-                    </span>
-                    <span id="question-title-4" class="question-title-hide">
-                        Напишите на иврите без огласовок
-                    </span>
-                    <span id="question-title-5" class="question-title-hide">
-                        Напишите на иврите с огласовками
-                    </span>
+                    <h3>
+                        <span id="question-title-1" class="question-title-hide">
+                            Переведите на русский язык
+                        </span>
+                        <span id="question-title-2" class="question-title-hide">
+                            Переведите на русский язык
+                        </span>
+                        <span id="question-title-3" class="question-title-hide">
+                            Переведите на иврит
+                        </span>
+                        <span id="question-title-4" class="question-title-hide">
+                            Напишите на иврите без огласовок
+                        </span>
+                        <span id="question-title-5" class="question-title-hide">
+                            Напишите на иврите с огласовками
+                        </span>
+                    </h3>
                     <p>
                         <span id="question-word">&nbsp;</span>
                         &#x200e;<span id="question-comment">&nbsp;</span>
@@ -54,13 +58,17 @@ require_once('parts/header.php');
                     </p>
                 </div>
                 <div id="stop">
-                    <span id="finished">Тест завершен</span>
+                    <h3>
+                        <span id="finished">Тест завершен</span>
+                    </h3>
                     <p id="buttons-stop">
                         <button id="button-save" class="btn primary">Записать результаты</button>
                     </p>
                 </div>
                 <div id="restart">
-                    <span id="saved">Результаты теста сохранены</span>
+                    <h3>
+                        <span id="saved">Результаты теста сохранены</span>
+                    </h3>
                     <p id="buttons-restart">
                         <button id="button-restart" class="btn primary">Загрузить новый тест</button>
                     </p>

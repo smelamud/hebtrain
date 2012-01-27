5360fdc01d429975fb9d27951024455772a8981f
Dmitry Melamuud <balu@migdal.ru>
2012-01-27 12:57:35 +0200

    Выпадающее меню тестов

--- parts/header.php
+++ parts/header.php
@@ -6,6 +6,8 @@ function displayPreamble($page, $bootstrapScripts = array()) {
     <link rel="stylesheet" type="text/css" href="/css/<?php echo $page; ?>.css"/>
     <script type="text/javascript"
             src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap-dropdown.js"></script>
+    <script type="text/javascript" src="/js/common.js"></script>
     <?php
     foreach ($bootstrapScripts as $script) {
         ?>
@@ -17,22 +19,43 @@ function displayPreamble($page, $bootstrapScripts = array()) {
     <?php
 }
 
-function displayMainMenuItem($current, $page, $title, $href) {
+function displayMainMenuItem($current, $page, $title, $href, $subMenu = array()) {
+    $classes = array();
     if ($current == $page) {
-        echo "<li class=\"active\"><a href=\"$href\">$title</a></li>";
-    } else {
-        echo "<li><a href=\"$href\">$title</a></li>";
+        $classes[] = 'active';
     }
+    if (count($subMenu) > 0) {
+        $classes[] = 'dropdown';
+    }
+    if (count($classes) > 0) {
+        echo "<li class=\"" . join(' ', $classes) . "\">";
+    } else {
+        echo '<li>';
+    }
+    if (count($subMenu) > 0) {
+        echo "<a href=\"$href\" class=\"dropdown-toggle\">$title</a>";
+        echo '<ul class="dropdown-menu">';
+        foreach ($subMenu as $item) {
+            displayMainMenuItem($current, $item[0], $item[1], $item[2]);
+        }
+        echo '</ul>';
+    } else {
+        echo "<a href=\"$href\">$title</a>";
+    }
+    echo '</li>';
 }
 
 function displayMainMenu($current) {?>
-    <div class="topbar">
+    <div class="topbar" data-dropdown="dropdown">
         <div class="fill">
             <div class="container">
                 <a class="brand" href="/">Иврит</a>
                 <ul class="nav"><?php
                     displayMainMenuItem($current, 'index', 'Начало', '/');
-                    displayMainMenuItem($current, 'run', 'Тест', '/run.php');
+                    displayMainMenuItem($current, 'run', 'Тест', '#', array(
+                        array('', 'Микс', '/run.php'),
+                        array('', 'Случайный', '/run.php')
+                    ));
                     displayMainMenuItem($current, 'items', 'Слова', '/items.php');
                 ?></ul>
             </div>

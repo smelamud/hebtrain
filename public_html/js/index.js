edb15323b4b745822a8ec8cb006ae4fc0fac8aba
Dmitry Melamuud <balu@migdal.ru>
2012-02-13 18:11:10 +0200

    Автоматическое обновление страницы статистики каждые 30 мин.

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -4,19 +4,21 @@ function loadStatistics() {
             $("#words-total").text(data.words_total);
             $("#questions-total").text(data.questions_total);
             $("#questions-now").text(data.questions_now);
+            $("#questions .item").remove();
             var template = $("#questions .template");
             $.each(data.questions, function(index, question) {
                 var newLine = template.clone();
-                newLine.removeClass("template");
+                newLine.removeClass("template").addClass("item");
                 newLine.find(".title").text(question.title);
                 newLine.find(".total").text(question.total);
                 newLine.find(".now").text(question.now);
                 template.before(newLine);
             });
+            $("#stages .item").remove();
             template = $("#stages .template");
             $.each(data.stages, function(index, stage) {
                 var newLine = template.clone();
-                newLine.removeClass("template");
+                newLine.removeClass("template").addClass("item");
                 newLine.find(".name").text(stage.name);
                 $.each(stage.steps, function(step, count) {
                     newLine.find(".count" + step).text(count);
@@ -29,6 +31,7 @@ function loadStatistics() {
             alert("Error!");
         }
     );
+    window.setTimeout(loadStatistics, 30 * 60 * 1000);
 }
 
 $(function() {

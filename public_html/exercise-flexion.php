8df0e483c0fb77010431b918214b575a1dd45214
Dmitry Melamuud <balu@migdal.ru>
2012-02-16 21:48:43 +0200

    Переделана страница exercise-flexion.

--- public_html/exercise-flexion.php
+++ public_html/exercise-flexion.php
@@ -13,7 +13,7 @@ require_once('parts/header.php');
             <div id="example">
                 <div id="progress"><span id="current"></span> из <span id="total"></span></div>
                 <div class="row">
-                    <div class="span1 offset5"><button id="prev-example" class="btn">&#x2190;</button></div>
+                    <div class="span1 offset3"><button id="prev-example" class="btn">&#x2190;</button></div>
                     <div id="example-text" class="span4"></div>
                     <div class="span1"><button id="next-example" class="btn">&#x2192;</button></div>
                 </div>

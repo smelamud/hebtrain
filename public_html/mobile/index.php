668c543a59a3fb2dfae504b561ae8255c0c654cf
Dmitry Melamuud <balu@migdal.ru>
2012-09-12 17:49:08 +0300

    Мобильная версия статистики.

---
+++ public_html/mobile/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+require_once('parts/header.php');
+?>
+<html>
+    <head>
+        <title>Иврит</title>
+        <?php displayPreamble('index'); ?>
+    </head>
+    <body class="mobile">
+        <div id="totals">
+            <a class="btn-large btn-primary" href="/mobile/run.php?qv=<?= QV_WORD_MIX ?>">Начать тест</a><br/>
+            <img id="spinner" src="/pics/ajax.gif"><br/>
+            <strong>Всего слов в словаре:</strong>
+            <span id="words-total">0</span>
+            (активных: <span id="words-active">0</span>)<br/>
+            <strong>Всего вопросов:</strong>&nbsp;
+            <span id="questions-total">&nbsp;</span>, сейчас -
+            <span class="questions-now">0</span>
+            (<span id="items-now">0</span> слов
+             = <span id="tests-now">0</span> тестов)<br/>
+            <strong>Вопросов в день:</strong>
+            <span id="questions-day">0</span>
+            = <span id="tests-day">0</span> тестов<br/>
+            <strong>Сегодня закончено тестов:</strong>
+            <span id="tests-done-today">0</span>
+        </div>
+    </body>
+</html>
